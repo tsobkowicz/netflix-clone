7f9734f389c6526c9f1138504c5fea6055ffb0d4
Tom <tomaszsobkowicz21@gmail.com>
2020-12-12 13:42:50 +0100

    Init project, basic cleanup,  configure typescript, eslint, prettier

---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import Jumbotron from './components/jumbotron';
+
+const App: React.FC = () => {
+  return <Jumbotron />;
+};
+
+export default App;
---
+++ src/components/jumbotron/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Jumbotron: React.FC = () => {
+  return <div>I am Jumbo!</div>;
+};
+
+export default Jumbotron;
